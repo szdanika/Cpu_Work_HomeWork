4e5ca3319103c86b02a824924ae3df99bc3feee4
szdanika <danikaszab@gmail.com>
2022-03-25 18:33:37 +0100

    Add project files.

---
+++ Cpu_Work_HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cpu_Work_HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
